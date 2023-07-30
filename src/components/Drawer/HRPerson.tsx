7737133df432a5641657de833c8f259430b6262e
akalanka <akalankapathirage22@gmail.com>
2023-07-31 01:12:26 +0530

    side drawer complete

--- src/components/Drawer/HRPerson.tsx
+++ src/components/Drawer/HRPerson.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@chakra-ui/react";
 import { VscRequestChanges } from "react-icons/vsc";
 import { useNavigate } from "react-router-dom";
+import { MdOutlineRateReview } from "react-icons/md";
 
 const HRSection: React.FC = () => {
   const naviagte = useNavigate();
@@ -12,6 +13,13 @@ const HRSection: React.FC = () => {
         onClick={() => naviagte("/review-registrations")}
       >
         Review Registration Requests
+      </Button>
+      <Button
+        leftIcon={<MdOutlineRateReview />}
+        variant="ghost"
+        onClick={() => naviagte("/create-schedules")}
+      >
+        Create Schedules
       </Button>
     </>
   );
--- src/components/Drawer/Manager.tsx
+++ src/components/Drawer/Manager.tsx
@@ -1,26 +1,52 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
-import { MdOutlineRateReview } from "react-icons/md";
+import {
+  MdOutlineRateReview,
+  MdDateRange,
+  MdInsertChart,
+  MdTimeline,
+} from "react-icons/md";
 import { VscRequestChanges } from "react-icons/vsc";
 import { useNavigate } from "react-router-dom";
 
 const ManagerSection: React.FC = () => {
-  const naviagate = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
       <Button
         leftIcon={<VscRequestChanges />}
         variant="ghost"
-        onClick={() => naviagate("/review-registrations")}
+        onClick={() => navigate("/review-registrations")}
       >
         Review Registration Requests
       </Button>
       <Button
         leftIcon={<MdOutlineRateReview />}
         variant="ghost"
-        onClick={() => naviagate("/review-leaves")}
+        onClick={() => navigate("/review-leaves")}
       >
         Review Leaves
+      </Button>
+      <Button
+        leftIcon={<MdTimeline />}
+        variant="ghost"
+        onClick={() => navigate("/review-timesheets")}
+      >
+        Review TimeSheets
+      </Button>
+      <Button
+        leftIcon={<MdDateRange />}
+        variant="ghost"
+        onClick={() => navigate("/review-schedules")}
+      >
+        Review Schedules
+      </Button>
+      <Button
+        leftIcon={<MdInsertChart />}
+        variant="ghost"
+        onClick={() => navigate("/create-report")}
+      >
+        Create Report
       </Button>
     </>
   );
